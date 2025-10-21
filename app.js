bc38b6dc90734a2689a8acc43f4873b57d01fe91
Fkarau <30944677+Karau1218@users.noreply.github.com>
2025-10-21 11:47:10 -0700

    working on it

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ const app = express();
 
 const PORT = 3000;
 
+// Enable static file serving
+app.use(express.static('public'));
 
 app.get('/', (req, res) =>{
-    res.sendFile(`${import.meta.dirname}/views/home.html`);
+    res.sendFile(`${import.meta.dirname}/index.html`);
 
+});
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
