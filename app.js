5a2ddd18c61bee3cc36b6a3d354b2513c5811c11
f00d <delossantos.orion@student.greenriver.edu>
2025-10-21 23:27:36 -0700

    finished

--- app.js
+++ app.js
@@ -4,43 +4,48 @@ const app = express();
 
 const PORT = 3000;
 
+const appointments = [];
+
 // Enable static file serving
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) =>{
-    res.sendFile(`${import.meta.dirname}index.html`);
+    res.sendFile(`${import.meta.dirname}/index.html`);
 
 });
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
-});
 
 
-//confirmation" route
-app.get('/confirm', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/confirmation.html`)
 
+//admin route
+app.get('/admin', (req, res) => {
+
+    res.send(appointments);
 })
 
 //POST ROUTE
 
-app.post('/submit-order', (req, res) => {
-    console.log(req.body);
-
+app.post('/submit-app', (req, res) => {
+const currentDate = new Date();
+const currentTime = `${currentDate.getTime()}`;
 // create a JSON object to store the data
 const appointmentSchedule = {
   fname: req.body.fname,
   lname: req.body.lname,
-  email: req.body.email,
   datein: req.body.datein,
-  timein: req.body.timein,  
- 
-};
+  timein: req.body.timein,
+  submission_time: currentTime
+ };
 
 // Add order to array
 appointments.push(appointmentSchedule);
 console.log(appointments);
 
-
+res.sendFile(`${import.meta.dirname}/confirmation.html`);
 
 })
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
---
+++ public/javascript.js
@@ -0,0 +1,27 @@
+document.getElementById('appointment-form').onsubmit = () => {
+    
+    clearErrors();
+
+    let isValid = true;
+
+    let fname = document.getElementById('fname').value.trim();
+    if (!fname) {
+        document.getElementById("err-fname").style.display = "block";
+        isValid = false;
+    }
+
+    let lname = document.getElementById('lname').value.trim();
+    if (!lname) {
+        document.getElementById("err-lname").style.display = "block";
+        isValid = false;
+    }
+
+    return isValid;
+}
+
+function clearErrors() {
+    let errors = document.getElementsByClassName("error");
+    for (let i=0; i<errors.length; i++) {
+        errors[i].style.display = "none";
+    }
+}
