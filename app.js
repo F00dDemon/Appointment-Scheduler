8d3f7641230544ddf70fc81784b0b09378fc9568
Fkarau <30944677+Karau1218@users.noreply.github.com>
2025-10-21 22:15:01 -0700

    worked on part 3

--- app.js
+++ app.js
@@ -8,9 +8,39 @@ const PORT = 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>{
-    res.sendFile(`${import.meta.dirname}/index.html`);
+    res.sendFile(`${import.meta.dirname}index.html`);
 
 });
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+
+//confirmation" route
+app.get('/confirm', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`)
+
+})
+
+//POST ROUTE
+
+app.post('/submit-order', (req, res) => {
+    console.log(req.body);
+
+// create a JSON object to store the data
+const appointmentSchedule = {
+  fname: req.body.fname,
+  lname: req.body.lname,
+  email: req.body.email,
+  datein: req.body.datein,
+  timein: req.body.timein,  
+ 
+};
+
+// Add order to array
+appointments.push(appointmentSchedule);
+console.log(appointments);
+
+
+
+})
